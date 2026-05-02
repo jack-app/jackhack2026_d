e047e257b1e40fabcb885fa3207f2623fc636a7d
kamiyoshi <o1.uun4y@icloud.com>
2026-05-02 17:51:56 +0900

    画像追加

--- src/components/WhitchNarrator.jsx
+++ src/components/WhitchNarrator.jsx
@@ -1,12 +1,13 @@
+import { backgroundBlurriness } from "three/tsl";
+import dummy from "../assets/witch.png";
 export default function WhitchNarrator({ lines }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'flex-end' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'flex-end' ,backgroundColor: '#0E1A20',}}>
       <div style={{ width: '100%' }}>
         {lines.map((text, index) => (
           <p key={index}>{text}</p>
         ))}
+        <img src={dummy} /></div>
       </div>
-      <div>🧙</div>
-    </div>
   );
 }
