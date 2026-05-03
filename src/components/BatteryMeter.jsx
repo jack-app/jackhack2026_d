6cfa2bb9bd424f501b5a714482d4e3254ed0053a
nk56_IdeapadS5G10_WSL <coding.nkato56@gmail.com>
2026-05-03 15:23:26 +0900

    バッテリー回復エフェクト

--- src/components/BatteryMeter.jsx
+++ src/components/BatteryMeter.jsx
@@ -2,10 +2,20 @@ import { forwardRef, useImperativeHandle, useEffect, useState } from 'react';
 
 const BatteryMeter = forwardRef(function BatteryMeter({ onBatteryChange }, ref) {
   let [battery, setBattery] = useState(100);
+  let [isRecovering, setIsRecovering] = useState(false);
 
   const increase = (n) => {
-    setBattery(prev => Math.min(prev + n, 100));
-    console.log("i");
+    const current = get();
+    if (current == 100) {
+      return;
+    } else {
+      setBattery(Math.min(current + n, 100));
+      console.log("i");
+      setIsRecovering(true);
+      setTimeout(() => {
+        setIsRecovering(false);
+      }, 400);
+    }
     // TODO: ここに回復時の演出などの追加機能を実装できます
   };
 
@@ -15,13 +25,15 @@ const BatteryMeter = forwardRef(function BatteryMeter({ onBatteryChange }, ref) 
     // TODO: ここにダメージ時の演出などの追加機能を実装できます
   };
 
-  const reset = () => setBattery(100);
+    const reset = () => setBattery(100);
+
+  const get = () => battery;
 
   useImperativeHandle(ref, () => ({
     increase,
     decrease,
     reset,
-    get: () => battery,
+    get
   }), [battery]);
 
   useEffect(() => {
@@ -33,11 +45,9 @@ const BatteryMeter = forwardRef(function BatteryMeter({ onBatteryChange }, ref) 
   const batteryLevelColor = battery <= 20  ? '#F44336' /*20以下*/ : battery <= 50 ? '#ffea00' /* 50以下 */ : '#17ff1b';
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 8, containerType:"inline-size", width: '100%', height: '100%', backgroundColor:"#0E1A20", zIndex:'-2'}}>
-      <button style={{ backgroundColor:'#ddd', height:'5%', aspectRatio:'1/1', zIndex:"100"}} onClick={() => console.log("a")}>+</button>
-      <button style={{ backgroundColor:'#ddd', height:'5%', aspectRatio:'1/1' }} onClick={() => decrease(10)}>-</button>
-      <p style={{ color:`${themeColor}`, fontFamily:"Noto Sans JP", fontWeight:"bold", fontSize:"1.3em" }}>Battery</p>
-      <div className="battery_container" style={{ position: 'relative', width: '40%', aspectRatio: "1/3" }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center', gap: 8, containerType:"inline-size", width: '100%', height: '100%', backgroundColor:"#0E1A20" , zIndex:'-2'}}>
+      <p style={{ color:`${themeColor}`, fontFamily:"Noto Sans JP", fontWeight:"bold", fontSize:"1.3em", userSelect:'none' }}>Battery</p>
+      <div className="whole_battery" style={{  position: 'relative', width: '36%', aspectRatio: "1/3" }}>
         <div className="battery_tip" style={{
           position: 'absolute',
           top: '-5%',
@@ -48,7 +58,7 @@ const BatteryMeter = forwardRef(function BatteryMeter({ onBatteryChange }, ref) 
           backgroundColor: `${themeColor}`,
           borderRadius: '3cqw 3cqw 0 0'
         }}/>
-        <div className="whole_battery" style={{
+        <div className="battery_container" style={{
           position: 'relative',
           display: 'flex', 
           alignItems: 'flex-end', 
@@ -67,13 +77,41 @@ const BatteryMeter = forwardRef(function BatteryMeter({ onBatteryChange }, ref) 
             opacity:"100%" , 
             borderRadius: '0 0 6cqw 6cqw', 
             transition: 'height 0.3s ease, background-color 0.3s ease', 
-            zIndex: '-1' 
+            zIndex: '-1'
+            
             }}/>
         </div>
+        <div className="recovery_effect_wrapper" style={{ position:'absolute', top:'0', left:'0', height:'100%', width:'100%', opacity: isRecovering ? 1 : 0, transition:'opacity 0.3s'}} >
+          <SingleRecoveryEffect translate={['380%', '-200%']}/>
+          <SingleRecoveryEffect translate={['-150%', '-100%']}/>
+          <SingleRecoveryEffect translate={['440%', '0%']}/>
+          <SingleRecoveryEffect translate={['-240%', '300%']}/>
+          <SingleRecoveryEffect translate={['390%', '500%']}/>
+          <SingleRecoveryEffect translate={['-100%', '500%']}/>
+        </div> 
       </div>
-      <p style={{ color: `${batteryLevelColor}`, fontFamily:"Noto Sans JP", fontWeight:"bold", fontSize:"1.4rem" }}>{battery}%</p>
+      <p style={{ color: `${batteryLevelColor}`, fontFamily:"Noto Sans JP", fontWeight:"bold", fontSize:"1.6rem", userSelect:'none' }}>{battery}%</p>
     </div>
   );
 });
+
+function SingleRecoveryEffect({ translate }) {
+  return (
+    <>
+      <p style={{color:'#17ff1b',
+        fontFamily:'Noto Sans JP',
+        fontWeight:'bolder',
+        fontSize:'1.6rem',
+        margin:'0',
+        height:'10%',
+        width:'30%',
+        textAlign:'center',
+        verticalAlign:'center',
+        userSelect:'none',
+        transform:`translate(${translate[0]}, ${translate[1]})`,
+      }}>+</p>
+    </>
+  );
+};
 
 export default BatteryMeter;
