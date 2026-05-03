7f7726cf2f11c4e556fde080455f2978f54ad1da
kamiyoshi <o1.uun4y@icloud.com>
2026-05-03 12:09:22 +0900

    画像を変えて吹き出しを追加しました。

--- src/components/WhitchNarrator.jsx
+++ src/components/WhitchNarrator.jsx
@@ -1,13 +1,19 @@
 import { backgroundBlurriness } from "three/tsl";
-import dummy from "../assets/witch.png";
+import dummy from "../assets/witch.png"
 export default function WhitchNarrator({ lines }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'flex-end' ,backgroundColor: '#0E1A20',}}>
       <div style={{ width: '100%' }}>
-        {lines.map((text, index) => (
+        <div style={{backgroundColor:'rgba(26, 48, 64, 0.6)' ,border: '2px solid #5DCAA5' 
+        , width: '70%'
+        , padding: '20px', borderRadius: '10px', margin: '5px', color: '#FFFF', fontSize: '18px', fontFamily: 'Arial, sans-serif'
+        }}>
+       {lines.map((text, index) => (
           <p key={index}>{text}</p>
         ))}
-        <img src={dummy} /></div>
+      </div>
+         <img src={dummy} 
+        style={{ width: "281px", height: "370px" }}/></div>
       </div>
   );
 }
